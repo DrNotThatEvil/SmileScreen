4a928d5382c48f81ebe25d4d890b2e3ca01b02a7
Willmar Knikker <wil7vin@live.nl>
2016-11-29 23:13:07 +0100

    Added a function to the template system

--- src/Template/TemplateSystem.php
+++ src/Template/TemplateSystem.php
@@ -1,11 +1,13 @@
 <?php 
 namespace SmileScreen\Template;
 
-
 use SmileScreen\Base\Singleton as Singleton;
 use SmileScreen\Config\ConfigFileSystem as ConfigFileSystem;
 use SmileScreen\Config\ConfigSystem as ConfigSystem;
 use SmileScreen\Exceptions\GenericSmileScreenException as GenericSmileScreenException;
+
+
+
 
 class TemplateSystem extends Singleton
 {
@@ -25,15 +27,40 @@ class TemplateSystem extends Singleton
     private function setTemplateDirectory(string $directory)
     {
         if (!is_dir($directory)) {
-            throw new GenericSmileScreenException('Could not load template directory: ' . $directory . ' Does it exist?');
+            throw new GenericSmileScreenException('Coudn\'t load template directory: ' .$directory. ' Does it exist?');
         }
 
         $this->fullTemplateDirectory = realpath($directory);
     }
 
+    public function asset(string $path)
+    {
+        $filename = basename($path);
+        $assetsFile = configFileSystem::getSiteRoot() . '/public/assets.json';
+        if (!file_exists($assetsFile)) {
+            return $path; 
+        }
+
+        $fileParts = explode('.', $filename);
+        $jsonFile = json_decode(file_get_contents($assetsFile), true);
+
+        if (!array_key_exists($fileParts[0], $jsonFile)) {
+            return $path; 
+        }
+
+        if (!array_key_exists($fileParts[1], $jsonFile[$fileParts[0]])) {
+            return $path; 
+        }
+
+        return $jsonFile[$fileParts[0]][$fileParts[1]];
+    }
+
     public function renderTemplate(array $vars, string $file)
     {
         extract($vars);
+
+        $template = $this;
+
         include $this->fullTemplateDirectory . '/' . $file;
     }
     
