6992c180b77f4e6a59386fe1585874deecfd2ae0
Willmar Knikker <wil7vin@live.nl>
2016-12-01 12:00:31 +0100

    Add delete model function

--- src/Database/DeleteQuery.php
+++ src/Database/DeleteQuery.php
@@ -5,5 +5,55 @@ use SmileScreen\Database\Query as Query;
 
 class DeleteQuery extends Query
 {
-     
+    protected $idField = 'id';
+
+    protected $id;
+
+    public function __construct() 
+    {
+        return $this;
+    }
+
+    public function setIdField(string $idField)
+    {
+        $this->idField = $idField; 
+        return $this;
+    }
+
+    public function setId(int $id)
+    {
+        $this->id = $id; 
+        return $this;
+    }
+
+    public function getid()
+    {
+        return $this->id;
+    }
+
+    public function isValid() 
+    {
+        return (isset($this->table) 
+            && isset($this->id)
+            && isset($this->idField));
+    }
+
+    public function getTable() 
+    {
+        return $this->table; 
+    }
+
+    public function getStatement() 
+    {
+        if(!$this->isValid()) {
+            return false; 
+        }
+
+        $statement = 'DELETE FROM ';
+        $statement .= $this->table;
+
+        $statement .= ' WHERE '.$this->idField.' = ?;';
+
+        return $statement;
+    }
 }
